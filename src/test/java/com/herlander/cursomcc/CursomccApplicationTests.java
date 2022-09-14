4856394821fc5443c1615599a2bbed0425aaefdf
Herlander <herlander97dasilva@gmail.com>
2022-09-14 11:11:22 -0300

    Projeto criado

---
+++ src/test/java/com/herlander/cursomcc/CursomccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herlander.cursomcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
