17ab3c4c6e2ce1a26354ede3fea82cf42b1ad97f
Herlander <herlander97dasilva@gmail.com>
2022-09-15 16:26:01 -0300

    Resta testado

--- src/main/java/com/herlander/cursomcc/CursomccApplication.java
+++ src/main/java/com/herlander/cursomcc/CursomccApplication.java
@@ -3,11 +3,14 @@ package com.herlander.cursomcc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CursomccApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomccApplication.class, args);
 	}
+
+
 
 }
