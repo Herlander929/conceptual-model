e4f9da88566127a5cdc79a6b8a24a8c42fe185b3
Herlander <herlander97dasilva@gmail.com>
2022-10-03 14:26:08 -0300

    Criando operação de instanciacao

--- src/main/java/com/herlander/cursomcc/CursomccApplication.java
+++ src/main/java/com/herlander/cursomcc/CursomccApplication.java
@@ -1,16 +1,31 @@
 package com.herlander.cursomcc;
 
+import com.herlander.cursomcc.domain.Categories;
+import com.herlander.cursomcc.repositories.CategorieRepositorie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 
 @SpringBootApplication
-public class CursomccApplication {
+public class CursomccApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategorieRepositorie categoriaRepositorie;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomccApplication.class, args);
 	}
 
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categories cat1 = new Categories(null, "Informática");
+		Categories cat2 = new Categories(null, "Escritório");
 
+		categoriaRepositorie.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
--- src/main/java/com/herlander/cursomcc/repositories/CategorieRepositorie.java
+++ src/main/java/com/herlander/cursomcc/repositories/CategorieRepositorie.java
@@ -4,6 +4,7 @@ import com.herlander.cursomcc.domain.Categories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface CategorieRepositorie extends JpaRepository<Categories, Integer> {
 
